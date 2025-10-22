c9b34e5910ea32d4a704e395840b1e5b453a35ef
lekshmisheelamurali <lekshmisheelamurali@gmail.com>
2025-10-22 12:04:36 +0530

    Update home.py

--- home.py
+++ home.py
@@ -7,8 +7,8 @@ st.markdown("Welcome! Choose a category below to calculate emissions:")
 st.page_link("pages/1_Fossil_Fuel_and_Firewood.py", label="🚗 Fossil Fuel and Firewood Emissions")
 st.page_link("pages/2_Paddy_Cultivation.py", label="🌾 Paddy Field Emissions")
 st.page_link("pages/3_Livestock_Emissions.py", label="🐄 Livestock Emissions")
-st.page_link("pages/4_Total_Emissions.py", label="📊 View Total Emissions Summary")
-st.page_link("pages/5_Electricity_Emissions.py", label="💡 Electricity Consumption Emissions")
+st.page_link("pages/5_Total_Emissions.py", label="📊 View Total Emissions Summary")
+st.page_link("pages/4_Electricity_Emissions.py", label="💡 Electricity Consumption Emissions")
 
 
 st.markdown("---")
